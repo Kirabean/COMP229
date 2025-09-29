34d8f4639d8239970c6f4a4193fd616f7aa6c069
Yasser <yasernabi110@gmail.com>
2025-09-28 21:51:49 -0400

    Working Portfolio

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,14 +5,14 @@ export default function Layout() {
   return (
     <>
       <header>
-        <a href="/"><img className="logo" src="public/YasserNabi.png" alt="Description of image" width="400" height="500"/></a>
+        <a href="/"><img className="logo" src="/YasserNabi.png" alt="Description of image" width="400" height="500"/></a>
         <nav>
           <Link to="/">Home</Link> | <Link to="/about">About</Link> | {" "}
           <Link to="/education">Education</Link> | {" "}
           <Link to="/project">Projects</Link> | {" "}
           <Link to="/contact">Contact</Link> | {" "}
           <Link to="/services">Services</Link>
-          <button className="resume-btn" onClick={() => window.open("src/assets/Yaser Nabi Resume.pdf", "_blank")}>Resume</button>
+          <button className="resume-btn" onClick={() => window.open("/Yaser Nabi Resume.pdf", "_blank")}>Resume</button>
         </nav>
         {/* <button onClick={() => window.open("src/assets/Yaser Nabi Resume.pdf", "_blank")}>Resume</button> */}
       </header>
